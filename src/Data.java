eff570d26aff6c1267e2830ac9874d48931fc152
Adomas Daškevičiaus <adomasda@gmail.com>
2024-08-23 15:20:58 +0300

    code formating - respecting the 80 char limit

--- src/Data.java
+++ src/Data.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 
 public class Data extends ArrayList<RVpair> implements Comparable<Data> {
     public Integer sum = 0;
-    Data(String name, String birthDay, Integer income, Integer workExp, Double debtRiskRatio) {
+    Data(String name, String birthDay, Integer income,
+         Integer workExp, Double debtRiskRatio) {
         super();
         this.add(new RVpair(name));
         this.add(new RVpair(LocalDate.parse(birthDay)));
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,21 @@ public class Main {
     public static void main(String[] args) {
         DataList dataList = new DataList();
 
-        dataList.addRow(new Data("Emily", "1992-05-22", 15000, 8, 0.3));
-        dataList.addRow(new Data("Carlos", "1978-08-11", 25000, 16, 0.65));
-        dataList.addRow(new Data("Aisha", "1995-03-09", 8000, 5, 0.2));
-        dataList.addRow(new Data("David", "1986-11-28", 13000, 12, 0.4));
-        dataList.addRow(new Data("Maria", "1970-06-17", 60000, 25, 0.7));
+        dataList.addRow(new Data(
+                "Emily", "1992-05-22",
+                15000, 8, 0.3));
+        dataList.addRow(new Data(
+                "Carlos", "1978-08-11",
+                25000, 16, 0.65));
+        dataList.addRow(new Data(
+                "Aisha", "1995-03-09",
+                8000, 5, 0.2));
+        dataList.addRow(new Data(
+                "David", "1986-11-28",
+                13000, 12, 0.4));
+        dataList.addRow(new Data(
+                "Maria", "1970-06-17",
+                60000, 25, 0.7));
 
         dataList.rateDataList().getRated();
     }
