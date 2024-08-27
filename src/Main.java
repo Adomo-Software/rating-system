d7da05a03b9bd5c3763fbfa144209abeeb02e247
Adomas Daškevičiaus <adomasda@gmail.com>
2024-08-27 19:32:56 +0300

    added summing column for each rating, rows are not sorted

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@ public class Main {
     public static void main(String[] args) {
         RateValueTable t = new RateValueTable();
 
-        t.setColumn("name", "jeff", "adam");
-        t.setColumn("age", 21, 22);
+        t.setColumn("Emily", "1992-05-22", 15000, 8, 0.3);
+        t.setColumn("Carlos", "1978-08-11", 25000, 16, 0.65);
+        t.setColumn("Aisha", "1995-03-09", 8000, 5, 0.2);
+        t.setColumn("David", "1986-11-28", 13000, 12, 0.4);
+        t.setColumn("Maria", "1970-06-17", 60000, 25, 0.7);
+//
+        t.rate();
 
         t.printTable();
+
     }
 }
--- src/TableFrame.java
+++ src/TableFrame.java
@@ -86,10 +86,59 @@ class Table extends TableFrame {
 }
 
 class RateValueTable extends Table {
+    Map<Map.Entry<String, Map<Integer, Object>>, Integer> sums;
+
     public RateValueTable () {
         super();
+        sums = new HashMap<>();
     }
     public void addColumn(String rowName, Object value) {
         super.addColumn(rowName, new RateValuePair(value));
+    }
+
+    public void rateColumn(Integer column) {
+        List<RateValuePair> list = new ArrayList<>();
+        for (Map.Entry<String, Map<Integer, Object>> rowEntry : super.rows.entrySet()) {
+            RateValuePair rvp = (RateValuePair) rowEntry.getValue().get(column);
+            list.add(rvp);
+        }
+
+        Collections.sort(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).rate = i + 1;
+        }
+    }
+
+    public void rate() {
+        for (int i = 0; i < columns.size(); i++) {
+            rateColumn(i);
+        }
+        for (Map.Entry<String, Map<Integer, Object>> rowEntry : super.rows.entrySet()) {
+            Integer acc = 0;
+            for (Map.Entry<Integer, Object> o : rowEntry.getValue().entrySet()) {
+                RateValuePair rvp = (RateValuePair) o.getValue();
+                acc += rvp.rate;
+            }
+            sums.put(rowEntry, acc);
+        }
+    }
+
+    @Override
+    public void printTable() {
+        for (Map.Entry<String, Map<Integer, Object>> rowEntry : rows.entrySet()) {
+            String rowName = rowEntry.getKey();
+            Map<Integer, Object> rowData = rowEntry.getValue();
+            StringBuilder rowOutput = new StringBuilder(String.format("%-10s", rowName));
+
+            for (int col : columns.keySet()) {
+                Object value = rowData.getOrDefault(col, " ");
+                rowOutput.append(String.format(" | %10s", value));
+            }
+            rowOutput.append(String.format(" | Total: %10s", sums.get(rowEntry)));
+
+
+            System.out.println(rowOutput);
+        }
     }
 }
